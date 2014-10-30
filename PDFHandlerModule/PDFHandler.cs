ffff5713cde27d6d4859f6e71d1a353d0a5e5532
Misato Takahashi <misato@takahashi.name>
2014-10-30 15:04:56 +0900

    Firstcommit

---
+++ PDFHandlerModule/PDFHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace PDFHandlerModule
+{
+    public class PDFHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public void ProcessRequest(HttpContext context)
+        {
+            var url = context.Request.CurrentExecutionFilePath;
+
+            if (string.IsNullOrEmpty(url))
+            {
+                return;
+            }
+
+            var res = HttpContext.Current.Response;
+            res.ClearContent();
+            res.ClearHeaders();
+            res.AddHeader("Content-Disposition", string.Format("filename={0}", url));
+            res.AddHeader("Content-Type", "application/pdf");
+            res.WriteFile(url);
+            res.End();
+
+        }
+    }
+}
+
---
+++ PDFHandlerModule/PDFHandlerModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PDFHandlerModule
+{
+    class PDFHandlerModule : IHttpModule
+    {
+        public void Dispose() { }
+        public void Init(HttpApplication context)
+        {
+        }
+    }
+}
